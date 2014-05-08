4b76fbaaa6d181aa8487956d0f822d4c7123053e
James Smith <james@floppy.org.uk>
2014-05-08 22:06:56 +0100

    cope with new-style foreman upstart files

--- definitions/foremanise.rb
+++ definitions/foremanise.rb
@@ -10,6 +10,16 @@ define :foremanise, :params => {} do
     )
   end
 
+  script 'Generate foreman path environment' do
+    interpreter 'bash'
+    cwd params[:cwd]
+    user params[:user]
+    code <<-EOF
+      RUBY="#{node[:rvm][:user_installs].select { |h| h[:user] == user }[0][:default_ruby]}"
+      echo "PATH=/home/#{user}/.rvm/gems/ruby-${RUBY}@global/bin:/home/#{user}/.rvm/rubies/ruby-${RUBY}/bin" >> /tmp/path_env
+    EOF
+  end
+
   script 'Start Me Up' do
     interpreter 'bash'
     cwd params[:cwd]
@@ -19,7 +29,7 @@ define :foremanise, :params => {} do
         BINPATH="/home/#{user}/.rvm/bin/:/home/#{user}/.rvm/rubies/ruby-${RUBY}/bin/"
         PATH=${BINPATH}:${PATH}
         export rvmsudo_secure_path=1
-        rvmsudo bundle exec foreman export upstart /etc/init
+        rvmsudo bundle exec foreman export upstart /etc/init -e #{params[:cwd]}/.env,/tmp/path_env
     EOF
   end
 end
