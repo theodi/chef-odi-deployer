645a28de849772ec8d7218e8ec874c0511a9dee6
pikesley <github@orgraphone.org>
2014-07-22 17:08:48 +0100

    Test migration has happened

--- test/integration/deployment-new-foreman/serverspec/localhost/deployment_spec.rb
+++ test/integration/deployment-new-foreman/serverspec/localhost/deployment_spec.rb
@@ -5,9 +5,12 @@ describe file("/var/www/certificates.theodi.org/current/config.ru") do
  it { should be_file }
 end
 
+describe command ('mysql -h localhost -u root -pilikerandompasswords certificate -e "show tables"') do
+  it { should return_stdout /surveys/i }
+end
+
 # Check we can actually access the thing - we'll get a Rails error due
 # to lack of database, but that's OK as we know Rails is running.
-# NOPE, THIS WON'T FLY IF WE'RE ATTEMPTING TO MYSQL-AS-A-SERVICE
 describe command("curl -H 'Host: certificates.theodi.org' localhost") do
   it { should return_stdout /Error - ODI Open Data Certificate/ }
 end
